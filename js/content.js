6c8e02e75ba9de4cc209be773245880ff8728196
Ralph Dittrich <r-dittrich@r-dittrich@netfutura.de>
2015-02-27 16:01:54 +0100

    Testphase und Erweiterung der neuen Funktion

--- js/content.js
+++ js/content.js
@@ -10,7 +10,15 @@ function setButtons() {
 	var tSaveButton = document.getElementById("syncstatusdone");
 
 	/* Define Buttons */
-	var tHelpButton = '<a href="javascript:void(tHelpButtonTrigger());" class="editbarbutton tHelp" title="Titanpad Color Expansion Help" unselectable="on">';
+	var tHelpButton = document.createElement('a');
+	tHelpButton.href = 'javascript:void(tHelpButtonTrigger())';
+	tHelpButton.className = 'editbarbutton tHelp';
+	tHelpButton.title = 'Titanpad Color Expansion Help';
+	tHelpButton.unselectable = 'on';
+	tHelpButton.style.display = 'block';
+	tHelpButton.style.position = 'absolute';
+	tHelpButton.style.height = '26px';
+	tHelpButton.style.width
 
 	/* Insert Buttons */
 	insertAfter(tHelpButton,tSaveButton);
@@ -18,29 +26,39 @@ function setButtons() {
 
 function tHelpButtonModal() {
 
-	var tHelpModal = '<div id="tHelpModal" class="tHiddenModal" style="display:none;position:absolute;width:100%;height:100%;z-index:800;">
-		<div id="tHelpModalInner">
-			<p><h1>Hilfe zu Titanpad Color Expansion Plugin</h1></p>
-			<p>
-				Aktuell gibt es <span style="color:red">4</span> Farbcodes<br/>
-				<br/>
-				Die aktuellen Farben lassen sich durch eine Anfangs- und Endmarkierung an ein Textstück setzen. Die Marker hierfür sind:<br/>
-				<ul>
-					<li>Einfache eckige Klammern für<span style="color:red">[roten Text]</span></li>
-					<li>Einfache Rauten für <span style="color:magenta">#magenta Text#</span></li>
-					<li>Doppelte Sterne für <span style="color:blue">**blauen Text**</span></li>
-					<li>Doppelte "einfache" Apostrophe (Shift + #) für <span style="color:green">\'\'grünen Text\'\'</span></li>
-				</ul>
-			</p>
-			<p>
-				Aktuelle Verion: 1.0.4<br/>
-				Author: Ralph Dittrich <a href="mailto:kizetu.rd@googlemail.com">kizetu.rd@googlemail.com</a><br/>
-				Lizenz: GNU GPL 02/2015
-			</p>
-		</div>
-	</div>';
+	var tMainmodals = document.getElementById('mainmodals');
 
-	insertAfter(tHelpModal,document.getElementById('mainmodals'));
+	var tHelpModal = document.createElement('div');
+	tHelpModal.id = 'tHelpModal';
+	tHelpModal.style.display = 'none';
+	tHelpModal.style.position = 'absolute';
+	tHelpModal.style.width = '100%';
+	tHelpModal.style.height = '100%';
+	tHelpModal.style.zIndex = '800';
+	tHelpModal.style.top = '0';
+	tHelpModal.style.left = '0';
+	tHelpModal.style.padding = '30% 10%';
+
+	tHelpModal.innerHTML = '<div id="tHelpModalInner" style="background-color:grey;">'+
+			'<a type="button" href="javascript:void(tHelpButtonTrigger());">Close</a>'+
+			'<p><h1>Hilfe zu Titanpad Color Expansion Plugin</h1></p>'+
+			'<p>'+
+				'Aktuell gibt es <span style="color:red">4</span> Farbcodes<br/>'+
+				'<br/>'+
+				'Die aktuellen Farben lassen sich durch eine Anfangs- und Endmarkierung an ein Textstück setzen. Die Marker hierfür sind:<br/>'+
+				'<ul>'+
+					'<li>Einfache eckige Klammern für<span style="color:red">[roten Text]</span></li>'+
+					'<li>Einfache Rauten für <span style="color:magenta">#magenta Text#</span></li>'+
+					'<li>Doppelte Sterne für <span style="color:blue">**blauen Text**</span></li>'+
+					'<li>Doppelte "einfache" Apostrophe (Shift + #) für <span style="color:green">\'\'grünen Text\'\'</span></li>'+
+				'</ul>'+
+			'</p>'+
+			'<p>'+
+				'Aktuelle Verion: 1.0.4<br/>Author: Ralph Dittrich <a href="mailto:kizetu.rd@googlemail.com">kizetu.rd@googlemail.com</a><br/>Lizenz: GNU GPL 02/2015'+
+			'</p>'+
+		'</div>';
+
+	insertAfter(tHelpModal,tMainmodals);
 }
 
 function tHelpButtonTrigger() {
