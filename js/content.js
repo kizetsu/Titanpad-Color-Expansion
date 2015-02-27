6fcce2cbdb73a1ff03d06fb91a3d637fa7b2d5e5
kizetsu <kizetsu.rd@googlemail.com>
2015-02-27 19:24:46 +0100

    small Bugfix

--- js/content.js
+++ js/content.js
@@ -1,6 +1,6 @@
 setButtons();
 tHelpButtonModal();
-setInterval(colors(), 1000);
+colors();
 
 function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
@@ -51,33 +51,35 @@ function tHelpButtonModal() {
 }
 
 function colors() {
-	var outerdocbody = document.getElementsByTagName("iframe")[1].contentWindow.document.getElementById('outerdocbody');
-	var innerdocbody = outerdocbody.getElementsByTagName("iframe")[0].contentWindow.document.getElementById("innerdocbody");
-
-	/* Markerset - add new marker here */
-	var markerRed = /\[.*\]/gi;
-	var markerMagenta = /\#.*\#/gi;
-	var markerBlue = /\*\*.*\*\*/gi;
-	var markerGreen = /\'\'.*\'\'/gi;
-	var etherArray = innerdocbody.getElementsByTagName("span");
-
-	for (index = 0; index < etherArray.length; ++index) {
-		var textItem = null;
-		textItem = etherArray[index].innerText;
-		
-		/* Markertest and color setting */
-		/* add new markertests here */
-		if(textItem.match(markerRed)) {
-			etherArray[index].style.color = "red";
+	setInterval(function() {
+		var outerdocbody = document.getElementsByTagName("iframe")[1].contentWindow.document.getElementById('outerdocbody');
+		var innerdocbody = outerdocbody.getElementsByTagName("iframe")[0].contentWindow.document.getElementById("innerdocbody");
+	
+		/* Markerset - add new marker here */
+		var markerRed = /\[.*\]/gi;
+		var markerMagenta = /\#.*\#/gi;
+		var markerBlue = /\*\*.*\*\*/gi;
+		var markerGreen = /\'\'.*\'\'/gi;
+		var etherArray = innerdocbody.getElementsByTagName("span");
+	
+		for (index = 0; index < etherArray.length; ++index) {
+			var textItem = null;
+			textItem = etherArray[index].innerText;
+			
+			/* Markertest and color setting */
+			/* add new markertests here */
+			if(textItem.match(markerRed)) {
+				etherArray[index].style.color = "red";
+			}
+			if(textItem.match(markerMagenta)) {
+				etherArray[index].style.color = "magenta";
+			}
+			if(textItem.match(markerBlue)) {
+				etherArray[index].style.color = "blue";
+			}
+			if(textItem.match(markerGreen)) {
+				etherArray[index].style.color = "green";
+			}
 		}
-		if(textItem.match(markerMagenta)) {
-			etherArray[index].style.color = "magenta";
-		}
-		if(textItem.match(markerBlue)) {
-			etherArray[index].style.color = "blue";
-		}
-		if(textItem.match(markerGreen)) {
-			etherArray[index].style.color = "green";
-		}
-	}
+	},1000);
 };
